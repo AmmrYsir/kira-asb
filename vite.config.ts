d9c091f01646d26b4c0f93f19cad045565eda600
AmmrYsir <ammaryasir03062000@gmail.com>
2026-02-03 10:59:44 +0800

    feat: integrate Tailwind CSS and update project configuration
    - Added Tailwind CSS as a dependency and configured it in vite.config.ts.
    - Created a new style.css file to import Tailwind CSS.
    - Updated App.tsx and index.tsx for consistent formatting.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+	plugins: [devtools(), solidPlugin(), tailwindcss()],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: 'esnext',
+	},
 });
